f4e0c95ff74d8a8273a1643754b7ad7d3f058586
liupei396 <729478581@qq.com>
2017-07-06 14:27:20 +0800

    count

--- count_server/src/main/java/com/chkd/count/common/utils/LogUtils.java
+++
@@ -1,9 +0,0 @@
-package com.chkd.count.common.utils;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class LogUtils {
-	
-	public static Logger log = LoggerFactory.getLogger(LogUtils.class);
-}
